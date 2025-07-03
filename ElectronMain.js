58ab9a37f80f38f332d0471f7b7adc0ce4b17ff7
Shengrui Liu <calmmanlsr@gmail.com>
2025-07-03 22:39:17 +0800

    加了一些新的基本配置, 为npx electron .做准备

--- ElectronMain.js
+++ ElectronMain.js
@@ -3,10 +3,13 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1920,
-    height: 1080
+    height: 1080,
+    webPreferences: {
+      webSecurity: false
+    }
   })
 
-  win.loadFile('index.html')
+  win.loadFile('build/index.html')
 }
 
 app.whenReady().then(() => {
